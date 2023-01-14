be04635ca3486dc6bbdb62b2d1b2a77df9f2769d
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-14 19:42:19 +0800

    Binary search tree

---
+++ BinarySearchTree.py
@@ -0,0 +1,15 @@
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# DATA STRUCTURES AND ALGORITHMS
+# PROGRAMMED BY: Nikka Pauline D. Generalao
+# BSCOE 2-2
+
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
