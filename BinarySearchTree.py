7dcdc08c4e0eaa751aee413ab4f487a041bd5e80
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-14 20:04:31 +0800

    search method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -39,6 +39,22 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                self.right.search(val)
+            else:
+                return False
+
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
@@ -50,4 +66,4 @@ def build_tree(elements):
 if __name__== '__main__':
     numbers = [17, 4, 1, 20, 9, 23, 18, 34]
     numbers_tree = build_tree(numbers)
-    print(numbers_tree.in_order_traversal())
+    print(numbers_tree.search(20))
